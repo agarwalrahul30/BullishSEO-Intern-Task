905e03f8bc47cee7d654f9ba31d88e16d27d3bc8
Rahul Agarwal <62092976+agarwalrahul30@users.noreply.github.com>
2022-01-14 22:11:46 +0530

    Add initial files

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//getting required elements
+const searchWrapper = $(".search_input");
+const inputBox = $("input");
+const suggBox = $(".autocom-box");
+
+//User presses any key and release
+inputBox.keyup ((e)=>{
+    if(e.which == 40 || e.which == 38) {
+        return;
+    }
+    let userData = e.target.value;
+    let emptyArray = [];
+
+    if(userData) {
+        //filtering array and user search to lowercase
+        emptyArray = suggestions.filter((data)=>{
+            //return only those names which start with user search
+            return data.name.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            var statusColor;
+            var phno;
+
+            if(data.status === "Appointment") {
+                statusColor = "green";
+            }else {
+                statusColor = "orange";
+            }
+
+            if(data.contact == null) {          //if patient does not give contact details
+                phno = "";
+            }else {
+                phno = data.contact;
+            }
+
+            return data = '<li> <div class="container"> <table cellspacing="8"> <tr> <td rowspan="3"> <img src="images/'+data.gender.toLocaleLowerCase()+'.png" width="65px"></td> <td>'+data.name+'</td> <td class="status '+statusColor+'">'+data.status+'</td> </tr> <tr> <td>| '+data.age+' | '+data.gender+' | '+phno+'</td> </tr> <tr> <td>| '+data.place+'</td> </tr> </table> </div> </li>';
+        });
+        searchWrapper.addClass("active");          //show the drop-down suggestions
+    }else {
+        searchWrapper.removeClass("active");       //hide it back
+    }
+    showSuggestions(emptyArray);
+  
+    $("li").click(function(e) {
+        $("input").val($(this).text().split(" ")[6]+" "+$(this).text().split(" ")[7]);
+    });// to fill in name
+});
+
+function showSuggestions(list) {
+    let listData = "";
+    let create;
+    if(list.length){
+        listData = list.join('');
+    }
+
+    userValue = inputBox.val();
+    create = '<li> <div class="container"> <table cellspacing="8"> <tr> <td id="create-cell">Create <span><span id="create">'+userValue+'</span><span></span></td> <td><span id="create-status">New</span></td> </tr> </table> </div> </li>';
+
+    listData+= create;          //if no suggestions match, this option will help add new person
+    
+    suggBox.html(listData);
+}
---
+++ js/suggestions.js
@@ -0,0 +1,154 @@
+const suggestions = [
+    {
+        name: "Rahul Agarwal",
+        age: 25,
+        gender: "Male",
+        contact: "9898986642",
+        place: "Port Blair",
+        status: "Appointment"
+    },
+    {
+        name: "Astha Singh",
+        age: 25,
+        gender: "Female",
+        contact: "789865423",
+        place: "Pune",
+        status: "Appointment"
+    },
+    {
+        name: "Ashok Gangwani",
+        age: 35,
+        gender: "Male",
+        place: "Rewa",
+        status: "Patient"
+    },
+    {
+        name: "Krishna Agarwal",
+        age: 25,
+        gender: "Male",
+        contact: "9933222111",
+        place: "Port Blair",
+        status: "Appointment"
+    },
+    {
+        name: "Anupam Agarwal",
+        age: 21,
+        gender: "Male",
+        contact: "9932019982",
+        place: "Port Blair",
+        status: "Appointment"
+    },
+    {
+        name: "Ayush Gupta",
+        age: 20,
+        gender: "Male",
+        contact: "9533526210",
+        place: "Meerut",
+        status: "Patient"
+    },
+    {
+        name: "Ishaan Gupta",
+        age: 20,
+        gender: "Male",
+        contact: "9898986642",
+        place: "Delhi",
+        status: "Patient"
+    },
+    {
+        name: "Dhruv Mittal",
+        age: 20,
+        gender: "Male",
+        contact: "7030291712",
+        place: "Delhi",
+        status: "Appointment"
+    },
+    {
+        name: "Karan Hemnani",
+        age: 25,
+        gender: "Male",
+        contact: "9278334642",
+        place: "Delhi",
+        status: "Appointment"
+    },
+    {
+        name: "Mohit Goel",
+        age: 25,
+        gender: "Male",
+        contact: "9323517192",
+        place: "Port Blair",
+        status: "Patient"
+    },
+    {
+        name: "Jais Sharma",
+        age: 36,
+        gender: "Male",
+        contact: "9892324542",
+        place: "Chennai",
+        status: "Appointment"
+    },
+    {
+        name: "Suvin Reddy",
+        age: 21,
+        gender: "Male",
+        contact: "9822346806",
+        place: "Chennai",
+        status: "Patient"
+    },
+    {
+        name: "Ram Nath",
+        age: 53,
+        gender: "Male",
+        contact: "7039372933",
+        place: "Kolkata",
+        status: "Patient"
+    },
+    {
+        name: "Varun Vaibhav",
+        age: 21,
+        gender: "Male",
+        contact: "9233738923",
+        place: "Dubai",
+        status: "Patient"
+    },
+    {
+        name: "Payal Choudhary",
+        age: 16,
+        gender: "Female",
+        contact: "9823455722",
+        place: "Kolkata",
+        status: "Appointment"
+    },
+    {
+        name: "Shlok Zanwar",
+        age: 21,
+        gender: "Male",
+        contact: "9222382344",
+        place: "Pune",
+        status: "Appointment"
+    },
+    {
+        name: "Kiran Bhattacharya",
+        age: 21,
+        gender: "Male",
+        contact: "9898228231",
+        place: "Mumbai",
+        status: "Patient"
+    },
+    {
+        name: "Aayushi Jethani",
+        age: 19,
+        gender: "Female",
+        contact: "9788782354",
+        place: "Chattisgarh",
+        status: "Patient"
+    },
+    {
+        name: "Ambika Choudhary",
+        age: 24,
+        gender: "Female",
+        contact: "9765656833",
+        place: "Bangalore",
+        status: "Appointment"
+    }
+
+]
